0de7c1b9e9fd451aaafaae9bcaa7a8bc03fa183d
karabo <mawaiskara@gmail.com>
2025-02-18 15:41:51 +0200

    created cards

--- uno_project.py
+++ uno_project.py
@@ -3,12 +3,30 @@ class cards:
         self.color = color
         self.c_set = c_set
 
-    def numbered_cards(self):
-        for x in range(1, 8):
-            self.c_set.append((x,self.color))
-            self.c_set.append((x,self.color))
+    def create_cards(self):
+        for x in range(0, 10):
+            if x !=0:
+                self.c_set.append((x,self.color))
+                self.c_set.append((x,self.color))
+            else:
+                self.c_set.append((x,self.color))
+        for x in range(2):
+            self.c_set.append(("reverse",self.color))
+            self.c_set.append(("skip",self.color))
+            self.c_set.append(("draw_2",self.color))
+            self.c_set.append(("wild",self.color))
+            self.c_set.append(("wild",self.color))
+            self.c_set.append(("wild_draw4",self.color))
+            self.c_set.append(("wild_draw4",self.color))
 
-    def sepecial_cards(self):
-        pass
+red_cards = cards("red")
+red_cards.create_cards()
+blue_cards = cards("blue")
+blue_cards.create_cards()
+green_cards = cards("green")
+green_cards.create_cards()
+yellow_cards = cards("yellow")
+yellow_cards.create_cards()
+print(green_cards.c_set)
 
     
