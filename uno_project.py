030f8501ee863ebc50733eb88d831c0a2a413f18
karabo <mawaiskara@gmail.com>
2025-02-23 16:26:26 +0200

    implemented logic for special cards

--- uno_project.py
+++ uno_project.py
@@ -1,3 +1,5 @@
+import time
+
 class cards:
     def __init__(self, color, c_set = []):
         self.color = color
@@ -19,6 +21,10 @@ class cards:
             self.c_set.append(("wild_draw4",self.color))
             self.c_set.append(("wild_draw4",self.color))
             return self.c_set
+red_cards = cards("red")
+blue_cards = cards("blue")
+green_cards = cards("green")
+yellow_cards = cards("yellow")
 
 class player():
     def __init__(self, name, hand=[]):
@@ -43,6 +49,7 @@ class player():
                 print("choose a valid number")
                 pick = int(input("choose by index: "))
         return pick
+karabo = player("karabo", hand=[])
 
 class computer:
     def __init__(self, name="npc", hand=[]):
@@ -56,26 +63,60 @@ class computer:
                 return x
         self.hand.append(drop_zone[-1])
         drop_zone.pop(-1)
+cpu = computer(hand=[])
 
 def revs_order(lst):
     return(lst[::-1])
-karabo = player("karabo")
-cpu = computer()
+
+
+def card_deck():
+    all_cards = green_cards.create_cards() + yellow_cards.create_cards() + red_cards.create_cards() + blue_cards.create_cards()
+    shuffe = list(set(all_cards))
+    return shuffe
+
+shuffled_cards = card_deck()
+drop_zone = []
+curse = 0
+
 def game(*players: object):
-    players_lst = [players]
-    print(revs_order(players_lst))
-    print(players_lst)
-    # red_cards = cards("red")
-    # blue_cards = cards("blue")
-    # green_cards = cards("green")
-    # yellow_cards = cards("yellow")
+    all_players = list(players)
+    start = input("start game? ")
+    if start.lower() != "yes":
+        return print("game was ended")
+    else:
+        print("game between player and computer")
+        print("dealing cards, please wait...")
+        for y in range(1, 8):
+            for x in all_players:
+                x.hand.append(shuffled_cards.pop())
+        drop_zone.append(shuffled_cards.pop())
+        time.sleep(4)
+        print(f"done!\ntop card is: {drop_zone[-1]}")
+        while True:
+            for x in all_players:
+                if drop_zone[-1][0] in ["draw_2", "skip", "reverse", "wild_draw4"] and curse > 0:
+                    if drop_zone[-1][0] == "draw_2":
+                        x.hand.append(shuffled_cards.pop())
+                        x.hand.append(shuffled_cards.pop())
+                        curse -= 1
+                    elif drop_zone[-1][0]  == "skip":
+                        continue
+                        curse -= 1
+                    elif drop_zone[-1][0]  == "reverse":
+                        all_players = revs_order(all_players)
+                        curse -= 1
+                    else:
+                        x.hand.append(shuffled_cards.pop())
+                        x.hand.append(shuffled_cards.pop())
+                        x.hand.append(shuffled_cards.pop())
+                        x.hand.append(shuffled_cards.pop())
+                        curse -= 1
+                else:
+                    pass
 
-    # def card_deck():
-    #     all_cards = green_cards.create_cards() + yellow_cards.create_cards() + red_cards.create_cards() + blue_cards.create_cards()
-    #     shuffe = list(set(all_cards))
-    #     return shuffe
 
-    # shuffled_cards = card_deck()
-    # drop_zone = []
-game(karabo,cpu)
+
+game(karabo,cpu
+
+)
     
